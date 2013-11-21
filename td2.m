b5ba75965bec55ecd9f0e5f334de35848321d46d
jeando <jean-do.favreau@hotmail.fr>
2013-11-21 15:24:22 +0100

    a

--- td2.m
+++ td2.m
@@ -11,9 +11,13 @@ function td2()
     img = img + b;
     
     n=11;
+    w=5*n;
     w=21;
     k=10;
-    alpha=10;
+    alpha=100;
+    
+    
+    
     img2=img;
     for x=n:size(img,2)-n
         for y=n:size(img,1)-n
@@ -23,17 +27,17 @@ function td2()
    % disp(strcat('x:',int2str(x),', y: ',int2str(y)));
     %disp(size(img));
             pref = patch_extract(img, n, x, y);
-            [ psim, weight] = similar_patches(img, w,x,y,pref,k,alpha);
-            un_sur_somme_w = 1/sum(weight);
-            weight=weight*un_sur_somme_w;
+  %          [ psim, weight] = similar_patches(img, w,x,y,pref,k,alpha);
+  %          un_sur_somme_w = 1/sum(weight);
+  %          weight=weight*un_sur_somme_w;
            % disp(un_sur_somme_w);
            % disp(psim);
            % disp(size(psim));
-            tmp = psim(1:n,1:n);
-            for i = 1 : k-1
-                tmp = tmp + psim(1:n,(i*n+1):((i+1)*n)) * weight(i+1);
-            end
-            img2(x,y) = tmp(6,6);
+   %         tmp = psim(1:n,1:n);
+   %         for ii = 1 : k-1
+   %             tmp = tmp + psim(1:n,(ii*n+1):((ii+1)*n)) * weight(ii+1);
+   %         end
+            img2(y,x) = similar_patches(img, n, w,x,y,pref,k,alpha);%tmp((n+1)/2,(n+1)/2);
         end
         disp(x);
     end
@@ -51,38 +55,36 @@ function td2()
        %disp(strcat('x:',int2str(x),', y: ',int2str(y)));
         pref = img((y-(n-1)/2):(y+(n-1)/2),( x-(n-1)/2):(x+(n-1)/2));
     end
-    function [ psim, weight] = similar_patches(img, w,x,y,pref,k,alpha)
-        n=size(pref,1);
+    %function [ psim, weight] = similar_patches(img, w,x,y,pref,k,alpha)
+    function [ val] = similar_patches(img, n, w,x,y,pref,k,alpha)
+        %n=size(pref,1);
       %  disp(n);
       %  all_patch = [];
-        all_norm = [];
-        weight = [];
-        psim = [];
+        n_rows = min(y+(w-1)/2,size(img,1)-(n-1)/2)-max(y-(w-1)/2,1+(n-1)/2);
+        all_norm = zeros((min(x+(w-1)/2,size(img,2)-(n-1)/2)-max(x-(w-1)/2,1+(n-1)/2))*n_rows,3);
+       % weight = [];
+      %  psim = [];
         for col = max(x-(w-1)/2,1+(n-1)/2) : min(x+(w-1)/2,size(img,2)-(n-1)/2)
+            ccols = col - max(x-(w-1)/2,1+(n-1)/2);
+            ccols_x_n_row = ccols*n_rows;
             for row = max(y-(w-1)/2,1+(n-1)/2) : min(y+(w-1)/2,size(img,1)-(n-1)/2)
-                %disp(size(img,2)-(n+1)/2);
-                tmp_patch = patch_extract(img,n,col,row);
-                tmp_norm = sum(sum((pref-tmp_patch).^2));
-                %all_patch = [all_patch, tmp_patch];
-                all_norm = [all_norm; [tmp_norm,row,col]];
+                rrows = row - max(y-(w-1)/2,1+(n-1)/2);
+                all_norm(ccols_x_n_row+rrows+1,:)=[sum(sum((pref-img((row-(n-1)/2):(row+(n-1)/2),( col-(n-1)/2):(col+(n-1)/2))).^2)),row,col];
             end
         end
-       % disp('toto');
-        %disp(size(all_norm));
-       % disp('toto2');
-        %disp(all_norm(1,:));
         [sorted_value, indice] = sort(all_norm(:,1));
         row = all_norm(indice,2);
         col = all_norm(indice,3);
-        %disp(col);
-        %disp('azer');
-        %disp(sorted_value);
-        %disp('azedfghjr');
+        tmp2 = zeros(n,n);
+        un_sur_alpha_nn = 1/(alpha * n * n);
+        weight = exp(-sorted_value(1:k)*un_sur_alpha_nn);
+        total_weight_inv = 1/sum(weight);
+        weight=weight*total_weight_inv;
         for i = 1 : k
-            weight = [weight, exp(-sorted_value(i)/(alpha*(n^2)))];
-            psim = [psim,patch_extract(img,n,col(i),row(i))];
+            tmp2 = tmp2 + patch_extract(img,n,col(i),row(i))*weight(i);
+            %psim(:,((i-1)*n+1):((i)*n)) = patch_extract(img,n,col(i),row(i));
         end
-        %disp(weight);
+        val = tmp2((n+1)/2,(n+1)/2);
     end
 end
 
